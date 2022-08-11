c7300a5e2b128463b0102de3b12513a09a046148
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-08-11 17:51:57 +0600

    chating server

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+const { Server } = require("socket.io")
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected ${socket.id}`);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with id: ${socket.id} joined room: ${data}`);
+    });
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    })
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    })
+});
+
+app.get('/', (req, res) => {
+    res.send('running test topgearperform')
+})
+
+server.listen(port, () => {
+    console.log("running server topgearperform");
+});
